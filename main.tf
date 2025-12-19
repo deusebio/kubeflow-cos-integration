3c252319d4233adabd360620875b6f84461d62e4
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-12-19 18:06:53 +0100

    Replace grafana agent with opentelemetry collector

--- main.tf
+++ main.tf
@@ -9,8 +9,7 @@ resource "juju_model" "cos" {
 
 module "cos" {
   depends_on = [juju_model.cos]
-  source     = "git::https://github.com/canonical/observability-stack//terraform/cos-lite"
-  # source = "./observability/terraform/modules/cos-lite"
+  source     = "git::https://github.com/canonical/observability-stack//terraform/cos-lite?ref=4199a1db2eda376fad1bd2ca8a4c9e109d0ac059"
   model = juju_model.cos.name
   channel = "1/stable"
   use_tls = false
@@ -22,21 +21,21 @@ resource "juju_model" "kubeflow" {
   cloud {
     name = var.K8S_CLOUD
   }
-  # config = {
-  #  juju-http-proxy = var.HTTP_PROXY
-  #  juju-https-proxy = var.HTTPS_PROXY
-  #   no-proxy = var.NO_PROXY
-  # }
+  config = {
+    juju-http-proxy = var.HTTP_PROXY
+    juju-https-proxy = var.HTTPS_PROXY
+    no-proxy = var.NO_PROXY
+  }
 }
 
 module "kubeflow_bundle" {
   depends_on = [juju_model.kubeflow]
-  source     = "git::https://github.com/canonical/charmed-kubeflow-solutions//modules/kubeflow-mlflow?ref=track/1.10"
+  source     = "git::https://github.com/canonical/charmed-kubeflow-solutions//modules/kubeflow-mlflow?ref=kf-8292-replace-grafana-agent-k8s"
   create_model = false
   cos_configuration = true
-  # http_proxy = var.HTTP_PROXY
-  # https_proxy = var.HTTPS_PROXY
-  # no_proxy = var.NO_PROXY
+  http_proxy = var.HTTP_PROXY
+  https_proxy = var.HTTPS_PROXY
+  no_proxy = var.NO_PROXY
 }
 
 
@@ -74,8 +73,8 @@ resource "juju_integration" "agent_loki" {
   model = module.kubeflow_bundle.model
 
   application {
-    name     = module.kubeflow_bundle.grafana_agent_k8s.app_name
-    endpoint = "logging-consumer"
+    name     = module.kubeflow_bundle.opentelemetry_collector_k8s.app_name
+    endpoint = "send-loki-logs"
   }
 
   application {
